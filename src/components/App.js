d9bba10f0b734af075cb98f8b64a6497e9181438
Th3c0d3d1 <bittradeguy007@gmail.com>
2024-09-25 17:16:37 -0400

    fetch proposal logic done; set states;

--- src/components/App.js
+++ src/components/App.js
@@ -7,26 +7,27 @@ import Navigation from './Navigation';
 import Loading from './Loading';
 
 // ABIs: Import your contract ABIs here
-import DAO_ABI from '../abis/Token.json'
+import DAO_ABI from '../abis/Dao.json'
 
 // Config: Import your network config here
 import config from '../config.json';
 
 function App() {
-  
+  const [provider, setProvider] = useState(null)
   const [dao, setDao] = useState(null)
-  
   const [treasuryBalance, setTreasuryBalance] = useState(0)
 
   const [account, setAccount] = useState(null)
 
-  const [proposal, proposals] = useState(null)
-
+  const [proposal, setProposals] = useState(null)
+  const [quorum, setQuorum] = useState(null)
+  
   const [isLoading, setIsLoading] = useState(true)
 
   const loadBlockchainData = async () => {
     // Initiate provider
     const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
 
     // Initiate Contracts
     const dao = new ethers.Contract(config[31337].dao.address, DAO_ABI, provider)
@@ -46,14 +47,14 @@ function App() {
     const count = await dao.proposalCount()
     const items = []
 
-    for(var i = 0; i < count; i++){
-
+    for(var i = 0; i < count; i++) {
       // Fetch Proposals
       const proposal = await dao.proposals(i + 1)
       items.push(proposal)
     }
-    proposals(items)
-    console.log(items)
+    setProposals(items)
+
+    setQuorum(await dao.quorum())
 
     setIsLoading(false)
   }
