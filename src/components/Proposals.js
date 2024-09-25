9c5ac8c818efaf915df8b605673236d0f9719bb4
Th3c0d3d1 <bittradeguy007@gmail.com>
2024-09-25 17:32:47 -0400

    Proposal table template done

--- src/components/Proposals.js
+++ src/components/Proposals.js
@@ -3,9 +3,34 @@ import Button from "react-bootstrap";
 import { ethers } from "ethers";
 
 const Proposals = ({ provider, dao, proposals, quorum, setIsLoading }) => {
+    console.log(proposals)
     return(
-        <>
-        </>
+        <Table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Proposal Name</th>
+                    <th>Recipient Addresss</th>
+                    <th>Amount</th>
+                    <th>Status</th>
+                    <th>Total Votes</th>
+                    <th>Cast Vote</th>
+                    <th>Finalize</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </Table>
     )
 }
 
