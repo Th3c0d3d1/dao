9dc3a5904f3963b097a3e743fdf8b809bb5a97b7
Th3c0d3d1 <bittradeguy007@gmail.com>
2024-09-25 15:52:29 -0400

    ABI & config import/ set DAO state/ ui adj

--- src/components/App.js
+++ src/components/App.js
@@ -7,14 +7,14 @@ import Navigation from './Navigation';
 import Loading from './Loading';
 
 // ABIs: Import your contract ABIs here
-// import TOKEN_ABI from '../abis/Token.json'
+import DAO_ABI from '../abis/Token.json'
 
 // Config: Import your network config here
-// import config from '../config.json';
+import config from '../config.json';
 
 function App() {
   const [account, setAccount] = useState(null)
-  const [balance, setBalance] = useState(0)
+  const [dao, setDao] = useState(null)
 
   const [isLoading, setIsLoading] = useState(true)
 
@@ -22,15 +22,14 @@ function App() {
     // Initiate provider
     const provider = new ethers.providers.Web3Provider(window.ethereum)
 
+    // Initiate Contracts
+    const dao = new ethers.Contract(config[31337].dao.address, DAO_ABI, provider)
+    setDao(dao)
+
     // Fetch accounts
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
     const account = ethers.utils.getAddress(accounts[0])
     setAccount(account)
-
-    // Fetch account balance
-    let balance = await provider.getBalance(account)
-    balance = ethers.utils.formatUnits(balance, 18)
-    setBalance(balance)
 
     setIsLoading(false)
   }
@@ -41,18 +40,16 @@ function App() {
     }
   }, [isLoading]);
 
-  return(
+  return (
     <Container>
       <Navigation account={account} />
 
-      <h1 className='my-4 text-center'>React Hardhat Template</h1>
+      <h1 className='my-4 text-center'>The DAO of the next generation!</h1>
 
       {isLoading ? (
         <Loading />
       ) : (
         <>
-          <p className='text-center'><strong>Your ETH Balance:</strong> {balance} ETH</p>
-          <p className='text-center'>Edit App.js to add your code here.</p>
         </>
       )}
     </Container>
