9b0ef8ebf0efd1f27a3fa46c0870507ab7848f9b
Th3c0d3d1 <bittradeguy007@gmail.com>
2025-07-04 13:22:40 -0400

    vote testing adj

--- test/DAO.js
+++ test/DAO.js
@@ -20,21 +20,32 @@ describe('DAO', () => {
         // Proposal Creator
         funder,
 
-        // HR Director
+        // HR Director/Staff
         admin,
+        staff,
 
-        // Marketing Director
+        // Marketing Director/Staff
         admin2,
+        staff2,
 
-        // Finance Director
+        // Finance Director/Staff
         admin3,
-        recipient,
-        user
+        staff3,
+
+        // Random Users
+        publicHolder,
+        user,
+        user2,
+
+        // Proposal Fund Recipient
+        proposalRecipient
 
     beforeEach(async () => {
 
         // Setup accounts
         accounts = await ethers.getSigners()
+
+        // CEO
         deployer = accounts[0]
 
         // Proposal Creator
@@ -44,32 +55,48 @@ describe('DAO', () => {
         admin = accounts[2]
         admin2 = accounts[3]
         admin3 = accounts[4]
-        recipient = accounts[5]
+        staff = accounts[5]
+        staff2 = accounts[6]
+        staff3 = accounts[7]
+        publicHolder = accounts[8]
 
         // Non-dao admin
-        user = accounts[8]
+        user = accounts[9]
+        user2 = accounts[10]
+
+        // Proposal Fund Recipient
+        proposalRecipient = accounts[11]
 
         // Deploy Tokens
         const Token = await ethers.getContractFactory('Token')
         token = await Token.deploy('Crypto Brosky Worldwide', 'CBW', '1200000')
 
         // Send tokens to investors
-        transaction = await token.connect(deployer).transfer(deployer.address, tokens(200000))
+        transaction = await token.connect(deployer).transfer(deployer.address, tokens(140000))
         await transaction.wait()
 
-        transaction = await token.connect(deployer).transfer(funder.address, tokens(200000))
+        transaction = await token.connect(deployer).transfer(funder.address, tokens(140000))
         await transaction.wait()
 
-        transaction = await token.connect(deployer).transfer(admin.address, tokens(200000))
+        transaction = await token.connect(deployer).transfer(admin.address, tokens(140000))
         await transaction.wait()
 
-        transaction = await token.connect(deployer).transfer(admin2.address, tokens(200000))
+        transaction = await token.connect(deployer).transfer(admin2.address, tokens(140000))
         await transaction.wait()
 
-        transaction = await token.connect(deployer).transfer(admin3.address, tokens(200000))
+        transaction = await token.connect(deployer).transfer(admin3.address, tokens(140000))
         await transaction.wait()
 
-        transaction = await token.connect(deployer).transfer(recipient.address, tokens(200000))
+        transaction = await token.connect(deployer).transfer(staff.address, tokens(140000))
+        await transaction.wait()
+
+        transaction = await token.connect(deployer).transfer(staff2.address, tokens(140000))
+        await transaction.wait()
+
+        transaction = await token.connect(deployer).transfer(staff3.address, tokens(140000))
+        await transaction.wait()
+
+        transaction = await token.connect(deployer).transfer(publicHolder.address, tokens(80000))
         await transaction.wait()
 
         // Deploy DAO
@@ -89,6 +116,10 @@ describe('DAO', () => {
             dao.add(admin.address),
             dao.add(admin2.address),
             dao.add(admin3.address),
+            dao.add(staff.address),
+            dao.add(staff2.address),
+            dao.add(staff3.address),
+            dao.add(publicHolder.address)
         ]);
 
         // Simulate voting start timestamps
@@ -116,7 +147,7 @@ describe('DAO', () => {
             beforeEach(async () => {
 
                 // args from createProposal() in contract
-                transaction = await dao.connect(funder).createProposal('Proposal 1', ether(100), recipient.address)
+                transaction = await dao.connect(funder).createProposal('Proposal 1', ether(100), proposalRecipient.address)
                 result = await transaction.wait()
             })
 
@@ -134,7 +165,7 @@ describe('DAO', () => {
                 // expected struct args
                 expect(proposal.id).to.eq(1)
                 expect(proposal.amount).to.eq(ether(100))
-                expect(proposal.recipient).to.eq(recipient.address)
+                expect(proposal.recipient).to.eq(proposalRecipient.address)
 
                 // view struct in terminal
                 // console.log(proposal)
@@ -142,115 +173,115 @@ describe('DAO', () => {
 
             it('emits a propose event', async () => {
                 await expect(transaction).to.emit(dao, 'Propose')
-                    .withArgs(1, ether(100), recipient.address, funder.address)
+                    .withArgs(1, ether(100), proposalRecipient.address, funder.address)
             })
         })
 
         describe('Failure', () => {
             it('rejects invalid amount', async () => {
-                await expect(dao.connect(admin).createProposal('Proposal', ether(1000), recipient.address)).to.be.reverted
+                await expect(dao.connect(admin).createProposal('Proposal', ether(1000), proposalRecipient.address)).to.be.reverted
             })
 
             it('rejects a non-investor', async () => {
-                await expect(dao.connect(user).createProposal('Proposal', ether(100), recipient.address)).to.be.reverted
+                await expect(dao.connect(user).createProposal('Proposal', ether(100), proposalRecipient.address)).to.be.reverted
             })
         })
     })
 
-    // describe('\nVoting', () => {
-    //     let
-    //         transaction,
-    //         result,
-    //         weight,
-    //         weight2,
-    //         weight3,
-    //         weight4,
-    //         ownershipTime
+    describe('\nVoting', () => {
+        let
+            transaction,
+            result,
+            weight,
+            weight2,
+            weight3,
+            weight4,
+            ownershipTime
 
-    //     beforeEach(async () => {
+        beforeEach(async () => {
 
-    //         // args from createProposal() in contract
-    //         transaction = await dao.connect(wluser).createProposal('Proposal 1', ether(100), recipient.address)
-    //         result = await transaction.wait()
-    //     })
+            // args from createProposal() in contract
+            transaction = await dao.connect(funder).createProposal('Proposal 1', ether(100), proposalRecipient.address)
+            result = await transaction.wait()
+        })
 
-    //     describe('Success', () => {
-    //         beforeEach(async () => {
+        describe('Success', () => {
+            beforeEach(async () => {
 
-    //             // Perform vote function
-    //             transaction = await dao.connect(wluser).vote(1, 2, wluser.address)
-    //             result = await transaction.wait()
-    //         })
+                // Perform vote function
+                transaction = await dao.connect(admin).vote(1, 2, admin.address)
+                result = await transaction.wait()
+            })
 
-    //         // Verify owner is on the whitelist
-    //         it('verifies owners whitelist status', async () => {
-    //             expect(await dao.isWhitelisted(deployer.address)).to.be.true
-    //         })
+            // Verify owner is on the whitelist
+            it('verifies owners whitelist status', async () => {
+                expect(await dao.isWhitelisted(deployer.address)).to.be.true
+            })
 
-    //         it('checks owners right to add/del user from whitelist ', async () => {
+            it('checks owners right to add/del user from whitelist ', async () => {
 
-    //             // Owner can add wluser to whitelist
-    //             await dao.add(investor2.address)
-    //             expect(await dao.isWhitelisted(investor2.address)).to.be.true
+                // Owner can add wluser to whitelist
+                await dao.add(user.address)
+                expect(await dao.isWhitelisted(user.address)).to.be.true
+                
+                // Verify wluser is on the whitelist
+                it('verifies wluser whitelist status', async () => {
+                    expect(await dao.isWhitelisted(user.address)).to.be.true
+                })
 
-    //             // Owner can remove user from whitelist
-    //             await dao.remove(wluser.address)
-    //             expect(await dao.isWhitelisted(wluser.address)).to.be.false
-    //         })
+                // Owner can remove user from whitelist
+                await dao.remove(user.address)
+                expect(await dao.isWhitelisted(user.address)).to.be.false
+            })
 
-    //         // Verify wluser is on the whitelist
-    //         it('verifies wluser whitelist status', async () => {
-    //             expect(await dao.isWhitelisted(wluser.address)).to.be.true
-    //         })
+            // Check for proposal votes count iteration
+            // Checking wluser balance
+            it('updates vote count', async () => {
+                const proposal = await dao.proposals(1)
+                expect(proposal.votes).to.eq(tokens(140000))
+            })
 
-    //         // Check for proposal votes count iteration
-    //         // Checking wluser balance
-    //         it('updates vote count', async () => {
-    //             const proposal = await dao.proposals(1)
-    //             expect(proposal.votes).to.eq(tokens(200000))
-    //         })
+            it('emits an event', async () => {
+                await expect(transaction).to.emit(dao, "Vote")
+                    .withArgs(1, 2, admin.address)
+            })
+        })
 
-    //         it('emits an event', async () => {
-    //             await expect(transaction).to.emit(dao, "Vote")
-    //                 .withArgs(1, 2, wluser.address)
-    //         })
-    //     })
+        describe('Failure', () => {
+            describe('Vote Start', async () => {
 
-    //     describe('Failure', () => {
-    //         describe('Vote Start', async () => {
+                // Simulate ownership start timestamps
+                await dao.connect(deployer).startVoting();
+            })
 
-    //             // Simulate ownership start timestamps
-    //             await dao.connect(deployer).startVoting();
-    //         })
+            it('rejects non-owner from adding/deleting whitelist users', async () => {
+                // expect the dao contract to be reverted if a non-owner tries to add a user to the whitelist
+                await expect(dao.connect(user).add(user2.address)).to.be.reverted
+                await expect(dao.connect(user2).add(user.address)).to.be.reverted
 
-    //         it('rejects non-owner from adding/deleting whitelist users', async () => {
-    //             // expect the dao contract to be reverted if a non-owner tries to add a user to the whitelist
-    //             await expect(dao.connect(investor4).add(investor2.address)).to.be.reverted
-    //             await expect(dao.connect(wluser).add(investor4.address)).to.be.reverted
+                // expect the dao contract to be reverted if a non-owner tries to remove a user from the whitelist
+                await expect(dao.connect(user).remove(admin.address)).to.be.reverted
+                await expect(dao.connect(user2).remove(admin.address)).to.be.reverted
+            })
 
-    //             // expect the dao contract to be reverted if a non-owner tries to remove a user from the whitelist
-    //             await expect(dao.connect(investor4).remove(investor2.address)).to.be.reverted
-    //             await expect(dao.connect(wluser).remove(investor4.address)).to.be.reverted
-    //         })
+            it('rejects non-wl investors from voting before time', async () => {
+                await expect(dao.connect(user).vote(1, 1, user.address)).to.be.reverted
+            })
 
-    //         it('rejects non-wl investors from voting before time', async () => {
-    //             await expect(dao.connect(user).vote(1, 1, user.address)).to.be.reverted
-    //         })
+            // Connect dao to user, submit 1 vote, revert as non-investor
+            it('rejects a non-investor', async () => {
+                await expect(dao.connect(user).vote(1, 1, user.address)).to.be.reverted
+            })
 
-    //         // Connect dao to user, submit 1 vote, revert as non-investor
-    //         it('rejects a non-investor', async () => {
-    //             await expect(dao.connect(user).vote(1, 1, user.address)).to.be.reverted
-    //         })
+            // Connect dao to staff, submit 1 vote, revert as double voter
+            it('rejects double voting', async () => {
+                transaction = await dao.connect(staff).vote(1, 1, staff.address)
+                await transaction.wait()
 
-    //         // Connect dao to wluser, submit 1 vote, revert as double voter
-    //         it('rejects double voting', async () => {
-    //             transaction = await dao.connect(wluser).vote(1, 1, wluser.address)
-    //             await transaction.wait()
-
-    //             await expect(dao.connect(wluser).vote(1, 1, wluser.address)).to.be.reverted
-    //         })
-    //     })
-    // })
+                await expect(dao.connect(staff).vote(1, 1, staff.address)).to.be.reverted
+            })
+        })
+    })
 
     // describe('\nGovernance', () => {
     //     let transaction, result
