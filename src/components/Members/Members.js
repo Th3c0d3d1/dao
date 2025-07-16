21d2b46aab2cf890e2ddb95b0623b3ceaed59755
Th3c0d3d1 <106117213+Th3c0d3d1@users.noreply.github.com>
2025-07-16 14:32:48 -0400

    Updated Members.js

--- src/components/Members/Members.js
+++ src/components/Members/Members.js
@@ -203,7 +203,7 @@ const Members = () => {
                                     </div>
                                     <div className="ml-3 min-w-0 flex-1">
                                         <h3 className="font-semibold text-gray-900 dark:text-gray-200 truncate">{config.title}</h3>
-                                        <p className="text-sm text-gray-600 truncate">Department Head</p>
+                                        <p className="text-sm text-gray-600 dark:text-gray-300 truncate">Department Head</p>
                                     </div>
                                 </div>
 
