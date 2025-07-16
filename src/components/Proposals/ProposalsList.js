4cd5066a1a56d09a6d12264431a4acc2c16c6543
Th3c0d3d1 <106117213+Th3c0d3d1@users.noreply.github.com>
2025-07-16 12:31:53 -0400

    Updated ProposalsList.js

--- src/components/Proposals/ProposalsList.js
+++ src/components/Proposals/ProposalsList.js
@@ -81,7 +81,7 @@ const ProposalsList = () => {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: index * 0.1 }}
-            className="bg-white rounded-lg p-3 lg:p-4 shadow-sm border border-gray-200 min-w-0"
+            className="light:bg-white dark:bg-gray-700 rounded-lg p-3 lg:p-4 shadow-sm border border-gray-200 min-w-0"
           >
             <div className="flex items-center">
               <div className={`p-1.5 lg:p-2 rounded-lg bg-${stat.color}-50 flex-shrink-0`}>
