83495798d7dd9397f1eb24215f10974f6f2c1a4f
Th3c0d3d1 <106117213+Th3c0d3d1@users.noreply.github.com>
2025-07-16 14:38:28 -0400

    Updated Members.js

--- src/components/Members/Members.js
+++ src/components/Members/Members.js
@@ -179,7 +179,7 @@ const Members = () => {
 
             {/* Department Level */}
             <div>
-                <h2 className="text-xl font-bold text-gray-900 dark:text-gray-200 shadow-sm border border-gray-200  mb-6 flex items-center">
+                <h2 className="text-xl font-bold text-gray-900 dark:text-gray-200 mb-6 flex items-center">
                     <UserGroupIcon className="w-6 h-6 mr-2 text-blue-600" />
                     Department Heads
                 </h2>
