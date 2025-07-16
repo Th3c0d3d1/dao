81b22b7586e16b71755a5d6da7c65d5a3f1c0e79
Th3c0d3d1 <106117213+Th3c0d3d1@users.noreply.github.com>
2025-07-16 12:44:02 -0400

    Updated Treasury.js

--- src/components/Treasury/Treasury.js
+++ src/components/Treasury/Treasury.js
@@ -326,7 +326,7 @@ const Treasury = () => {
         </div>
 
         <div className="overflow-x-auto min-w-0">
-          <table className="w-full light:bg-white dark:bg-gray-700 ">
+          <table className="w-full">
             <thead className="bg-gray-50">
               <tr>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
@@ -346,7 +346,7 @@ const Treasury = () => {
                 </th>
               </tr>
             </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
+            <tbody className="light:bg-white dark:bg-gray-700 divide-y divide-gray-200">
               {loading ? (
                 <tr>
                   <td colSpan="5" className="px-6 py-12 text-center">
